8ae389b02a6cd70fd04a285860f9f14a12123304
victoriadeleon <A01656328@tec.mx>
2022-03-24 12:49:00 -0600

    Se alinearon números

--- memory.py
+++ memory.py
@@ -81,9 +81,10 @@ def draw():
     if mark is not None and hide[mark]:
         x, y = xy(mark)
         up()
-        goto(x + 2, y)
+        # se centró texto modificando valores sumados a x y y
+        goto(x + 25, y+ 2)
         color('black')
-        write(tiles[mark], font=('Arial', 30, 'normal'))
+        write(tiles[mark], font=('Arial', 30, 'normal'), align = "center")
 
     update()
     ontimer(draw, 100)
